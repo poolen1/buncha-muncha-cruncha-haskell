8a2ea79133e5cddf587e853e2570c8acf2a3810d
Nick Poole <42500386+poolen1@users.noreply.github.com>
2023-03-25 11:53:11 -0700

    List comprehension predicate example

--- bmch/baby.hs
+++ bmch/baby.hs
@@ -1,3 +1,5 @@
+import Language.Haskell.TH
+
 doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
@@ -5,3 +7,5 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
     then x
     else x*2
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
