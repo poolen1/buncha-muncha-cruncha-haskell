b2e874d2bb502cda422a3289da6c9bd7387954c7
Nick Poole <42500386+poolen1@users.noreply.github.com>
2023-03-25 11:59:42 -0700

    Tuples and list comprehension together

--- bmch/baby.hs
+++ bmch/baby.hs
@@ -9,3 +9,6 @@ doubleSmallNumber x = if x > 100
     else x*2
 
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], 
+    a^2 + b^2 == c^2, a+b+c == 24]
